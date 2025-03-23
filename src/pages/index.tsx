defc864924eaf070c8d2f76716888c75492dfd4d
HuertRa3490 <rhuertas@DESKTOP-PALVNBI.>
2025-03-23 09:17:00 -0500

    dev(removepil): Changes tested

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,47 +1,69 @@
 import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Canvas} from "@react-three/fiber";
+import {Box} from "./keyboard/keyboard";
+import { OrbitControls, Sky } from "@react-three/drei";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+import React, { useEffect, useState } from "react";
+import { KeyboardLayout } from "./keyboard/core";
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const loadLayout=(filename:string, setLayout:any)=>{
+    fetch(filename).then(
+      function(response){
+        console.log("raw data "+response)
+        return response.json()
+      }
+    ).then(
+      function(jsonData){ 
+          const newLayout = KeyboardLayout.fromJSON(jsonData.layout)
+          console.log(newLayout)
+          setLayout(newLayout)
+      }
+    )
+
+
+}
+
 
 export default function Home() {
+  const [layout,setLayout] = useState( new KeyboardLayout())
+
+  useEffect(()=>{
+    loadLayout("keyboardlayouts/nutymini.json",setLayout)
+  },[])
+
+
+  console.log("render")
+  console.log(layout.keys.length)
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/pages/index.tsx
-            </code>
-            .
-          </li>
-        </ol>
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://www.nutylabs.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://www.apple.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+
+      <Canvas orthographic camera ={ {
+            position:[0,0,100],
+            zoom:1,
+            left:layout.minX()*1.5,
+            right:layout.maxX()*1.5,
+            top:layout.maxY(),
+            bottom:layout.minY()
+            }
+        }>
+        <OrbitControls></OrbitControls>
+        <ambientLight intensity={0.5}></ambientLight> 
+        <Sky sunPosition={[100, 10, 100]}></Sky>
+        <Box position={[-9.5, 0, 0]} > </Box>
+        <Box position={[ 9.5, 0, 0]} > </Box>
+        {
+          layout.keys.map((keyProps : Float32Array[],index:number) => {
+            return  <Box position={[keyProps[0],keyProps[1],0]} key={index}  ></Box>
+          }) 
+        }
+      </Canvas>
+
+
+
+
+
+
       </main>
     </div>
   );
